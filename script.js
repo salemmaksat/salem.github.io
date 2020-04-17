9b02d84014bec1a038b126aa426bea7a509881ab
salemmaksat <63857763+salemmaksat@users.noreply.github.com>
2020-04-17 20:53:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -48,10 +48,10 @@ jQuery(document).ready(function() {
         emailjs.send('gmail', 'base', templateParams)
             .then(function(response) {
                console.log('SUCCESS!', response.status, response.text);
-               success = true
+               success = true;
             }, function(error) {
                console.log('FAILED...', error);
-               success = false
+               success = false;
             });
 
         if(success) {
